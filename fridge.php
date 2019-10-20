2b9e8172dfc1bc70b775140f060e9cf4e84b3ab1
=Galiixy <>
2019-10-20 21:22:15 +0200

    fridge fonctionne!

--- fridge.php
+++ fridge.php
@@ -1,9 +1,31 @@
 <?php
 session_start();
 
-include['maquette/baseConnection.php'];
+#include['maquette/baseConnection.php'];
+$host ='localhost';
+$db ='galiixy';
+$user='galiixy';
+$pass='Jobslpxi';
+$charset='utf8mb4';
 
- 
+$dsn ="mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES =>false,
+];
+try {
+	$pdo = new PDO($dsn,$user,$pass,$options);
+} catch(\PDOException $e){
+	throw new \PDOException($e->getMessage(),(int)$e->getCode());
+}
+
+
+
+$res =$pdo->query("Select f.name as Name, f.nutriction_fact as Nutriction from Food_Definition f,Food food,User u
+WHERE u.login='$_SESSION[login]'
+AND u.Id_User=food.Id_User
+AND food.Id_Food=f.Id_Food");
 ?>
 
 <?php include ['maquette/header.php'];?>
@@ -18,13 +40,23 @@ include['maquette/baseConnection.php'];
     </t>
 
 <?php
-    while($row=$res->fetch())
-    {
+if(isset($row)) {
+	while($row=$res->fetch())
+    {?>
     <tr>
         <td> <?php echo $row['Name'] ?> </td>
         <td> <?php echo $row['Nuctriction'] ?> </td>
     </tr>
-    }
+<?php  }
+}
+else{ ?>
+<tr> 
+	<td> aucune nourriture ajouté </td> 
+</tr> 
+
+<?php
+
+}
 
 
 ?>
