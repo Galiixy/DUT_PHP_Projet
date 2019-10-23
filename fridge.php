a44bc21802932870f9b34ec6584e3953a40f1443
noelle <>
2019-10-23 16:41:28 +0200

    modification fridge vision storage

--- fridge.php
+++ fridge.php
@@ -22,45 +22,41 @@ try {
 
 
 
-$res =$pdo->query("Select f.name as Name, f.nutriction_fact as Nutriction from Food_Definition f,Food food,User u
+$res =$pdo->query("Select f.name as Name,food.quantity as Quantity, f.nutriction_fact as Nutriction from Food_Definition f,Food food,User u
 WHERE u.login='$_SESSION[login]'
 AND u.Id_User=food.Id_User
 AND food.Id_Food=f.Id_Food");
 ?>
 
-<?php include ['header.php'];?>
+<?php include ('maquette/header.php');?>
 
-<table align="center" border="1px" style ="width:300px; line-height:30px;">
-    <tr>
-        <th> <h2>Fridge</h2></th>
-    </tr>
-    <t>
-        <th>Food</th>
-        <th>Nutriction Fact</th>
-    </t>
+<div id="storage"> 
+	<table align="left" border="1px" style ="width:300px; line-height:30px;">
+    		<tr>
+        		<th colspan=3> <h2>Fridge</h2></th>
+    		</tr>
+    		<t>
+     	   		<th>Food</th>
+			<th>Nutriction Fact</th>
+			<th>Quantity </th>
+		</t>
 
-<?php
-if(isset($row)) {
-	while($row=$res->fetch())
-    {?>
-    <tr>
-        <td> <?php echo $row['Name'] ?> </td>
-        <td> <?php echo $row['Nuctriction'] ?> </td>
-    </tr>
-<?php  }
-}
-else{ ?>
-<tr> 
-	<td> aucune nourriture ajouté </td> 
-</tr> 
-
-<?php
-
-}
-
-
-?>
-
-
-</table>
-<?php include ['footer.php']?>
+	<?php
+		if($row=$res->fetch()){
+			do{?>
+			<tr>
+       				<td> <?php echo $row['Name'] ?> </td>
+				<td> <?php echo $row['Nutriction'] ?> </td>
+				<td> <?php echo $row['Quantity'] ?> </td>
+   	 		</tr>
+			<?php }	while($row=$res->fetch());
+			} else{?>
+			<tr>
+			<td colspan=3> No food here ! Please add some food !</td>
+			</tr> <?php }?>
+	</table>
+</div> 
+<div id="add">
+	<h1>Ajout d'aliments</h1>	
+</div>
+<?php #include ('maquette/footer.php')?>
