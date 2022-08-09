eebc783cae361ca5f82dd33e5a820ea86444637a
Galiixy <50149050+Galiixy@users.noreply.github.com>
2022-08-09 16:16:16 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>Index</title>
-<!--<link rel="stylesheet" href="style.css">-->
-</head>
-<body>
+
 	<header>
 		<h1>Bienvenue sur Cookingly</h1>
 	</header>
@@ -18,5 +11,3 @@
             <li><a href="signOut.php">Sign out</a></li>
         </ul> 
 	</nav>
-</body>
-</html>
